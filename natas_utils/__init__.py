d26c7a3ce398d7d7aa25fd2d574c0e176229c12d
Evan Johnson <evanj2357@gmail.com>
2021-06-21 10:50:09 -0700

    clean up formatting

--- natas_utils/__init__.py
+++ natas_utils/__init__.py
@@ -1,3 +1,12 @@
-from .natas_utils import PW_FORMAT, extract_candidate_passwords, flag_file_abspath, load_level, store_level_password, try_level_login
+from .natas_utils import (
+    PW_FORMAT,
+    LevelLogin,
+    LevelData,
+    extract_candidate_passwords,
+    flag_file_abspath,
+    load_level,
+    store_level_password,
+    try_level_login
+)
 
 NATAS_DATA = dict.copy(natas_utils.NATAS_DATA)
