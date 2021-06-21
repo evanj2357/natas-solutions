69f05eba2c1ef7ec62e8cab6d06b93250d45070d
Evan Johnson <evanj2357@gmail.com>
2021-06-21 10:52:23 -0700

    refactor for use in main script

--- solutions/natas01.py
+++ solutions/natas01.py
@@ -2,17 +2,15 @@
 natas1: view source, check comments
 """
 
-import json
 import requests
 from bs4 import BeautifulSoup
 
 from natas_utils import *
 
 LEVEL = 1
-URL, LOGIN = load_level(LEVEL)
 
-def main():
-    response = requests.get(URL, auth=LOGIN)
+def solve(url: str, login: LevelLogin):
+    response = requests.get(url, auth=login)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
@@ -25,9 +23,14 @@ def main():
     # line from the source file
     # password is the last "word" in a comment after one line of text
     natas2_password = content.contents[1].strip().split()[-1]
-    print("natas2:", natas2_password)
 
-    store_level_password(LEVEL + 1, natas2_password)
+    return natas2_password
+
 
 if __name__ == "__main__":
-    main()
+    url, login = load_level(LEVEL)
+    natas2_password = solve(url, login)
+
+    if natas2_password:
+        print("natas2:", natas2_password)
+        store_level_password(LEVEL + 1, natas2_password)
