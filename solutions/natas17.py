781c071101529d55f02d4412eccfa0ad7001b750
Evan Johnson <evanj2357@gmail.com>
2021-06-21 14:10:40 -0700

    add skeletons for remaining levels to solve

---
+++ solutions/natas17.py
@@ -0,0 +1,22 @@
+"""
+"""
+
+import requests
+from typing import Optional
+
+from natas_utils import *
+
+LEVEL = 17
+
+def solve(url: str, login: LevelLogin) -> Optional[str]:
+    return None
+
+if __name__ == "__main__":
+    url, login = load_level(LEVEL)
+    flag = solve(url, login)
+
+    if flag:
+        print("natas{:02}:".format(LEVEL), flag)
+        store_level_password(LEVEL + 1, flag)
+    else:
+        exit("Failed to get password for natas{:02}.".format(LEVEL))
