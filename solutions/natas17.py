5497eb8006b322b4d6ddee50cc5fb41028a86fdf
Evan Johnson <evanj2357@gmail.com>
2021-06-23 10:43:51 -0700

    initial interaction

--- solutions/natas17.py
+++ solutions/natas17.py
@@ -9,6 +9,21 @@ from natas_utils import *
 LEVEL = 17
 
 def solve(url: str, login: LevelLogin) -> Optional[str]:
+    # form action: POST to /index.php (can be /)
+    #
+    # The server code querying is not POST-specific, so params on a GET should
+    # work.
+    #
+    # A non-null "debug" param on a GET request will leak info about the query
+    # that's executed.
+    params = {
+        "username": "",
+        "submit": "Check existence",
+        "debug": "true",
+    }
+    response = requests.get(url, auth=login, params=params)
+    print(response.text)
+
     return None
 
 if __name__ == "__main__":
